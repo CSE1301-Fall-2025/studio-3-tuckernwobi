fe5d5800dc806f1b04ebd0081582e3a90e38eba6
AndrewTucker18 <a.j.tucker@wustl.edu>
2025-09-17 15:23:54 -0500

    Finished

--- src/Sieve.java
+++ src/Sieve.java
@@ -5,11 +5,25 @@ public class Sieve {
         Scanner scanInput = new Scanner(System.in);
         System.out.print("Search for prime numbers up to what number: ");
         int n = scanInput.nextInt();
-        boolean[] number = boolean boolean[n+1];
-        for(int i = 2; i < n+1; i++) {
+        boolean[] number = new boolean[n+1];
+        for(int i = 2; i <= n; i++) {
             number[i] = true;
-            System.out.println(number[i]);
         }
+        for(int i = 0; i<=n; i++){
+            if (number[i] == true){
+                for(int j = i + i; j <= n; j=j+i){
+                    number[j] = false;
+                } 
+            }
+        }
+        System.out.println("These are the prime numebrs until " + n + ": ");
+        for(int i = 0; i<=n; i++) {
+            if (number[i] == true){
+                System.out.print(" " + i);
+            }
+        }
+
+
     }
 
 
